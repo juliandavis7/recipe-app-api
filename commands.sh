015906c7cf3c297eae3f87359904455b2d4457c4
juldavis <juldavis@deloitte.com>
2023-02-07 11:47:55 -0800

    password too short test failing

--- commands.sh
+++ commands.sh
@@ -1,11 +1,20 @@
 # build container
 docker-compose build
 
-# run app
+# start app; run all containers
 docker-compose up
 
-# terminate all running containers
+# stop app; terminate all containers
 docker-compose down
 
 # run tests
 docker-compose run --rm app sh -c "python manage.py test"
+
+# make migrations
+docker-compose run --rm app sh -c "python manage.py makemigrations"
+
+# migrate
+docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"
+
+# delete DB
+docker volume rm recipe-app-api_dev-db-data
