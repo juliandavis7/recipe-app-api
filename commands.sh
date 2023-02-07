de90dabe2e5717463cb98f55c23855001bfe27ec
juldavis <juldavis@deloitte.com>
2023-02-06 18:36:49 -0800

    configured docker compose and checks to wait for db

--- commands.sh
+++ commands.sh
@@ -1,5 +1,11 @@
+# build container
+docker-compose build
+
 # run app
 docker-compose up
+
+# terminate all running containers
+docker-compose down
 
 # run tests
 docker-compose run --rm app sh -c "python manage.py test"
